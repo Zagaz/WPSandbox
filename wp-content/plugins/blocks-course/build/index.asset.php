d5997f7c737503f52164cd1c23803152543ec553
Zagaz <andre_ranulfo@hotmail.com>
2023-12-05 16:37:29 -0300

    Alignment Toolbar

--- wp-content/plugins/blocks-course/build/index.asset.php
+++ wp-content/plugins/blocks-course/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '693d9e6e90574471ea70');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '292da54b32d49a7a255a');
