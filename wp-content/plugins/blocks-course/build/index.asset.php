eac38c38d33ffd71d3a454736f9bb6653f7cb55b
Zagaz <andre_ranulfo@hotmail.com>
2023-12-05 13:59:59 -0300

    RichText

--- wp-content/plugins/blocks-course/build/index.asset.php
+++ wp-content/plugins/blocks-course/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'b3665292b17a46daa4f8');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'c2253d6f12f3b10f5f24');
--- wp-content/plugins/blocks-course/src/index.js
+++ wp-content/plugins/blocks-course/src/index.js
@@ -3,8 +3,6 @@ import './style.scss';
 import Edit from './edit';
 import save from './save';
 import metadata from './block.json';
-import Icon from './icon';
-
 
 registerBlockType( metadata.name, {
 	icon: {
