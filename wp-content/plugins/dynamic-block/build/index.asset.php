bafbe435384fc426e302c052d8ba2d4afd7308a7
Zagaz <andre_ranulfo@hotmail.com>
2023-12-13 12:32:54 -0300

    Minors Changes

--- wp-content/plugins/dynamic-block/build/index.asset.php
+++ wp-content/plugins/dynamic-block/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n'), 'version' => '4b604de225b02e5ef5d6');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n'), 'version' => 'b5d290e05fa5cebb1d1b');
